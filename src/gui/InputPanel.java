d0b1ee02e1a8aa81a0aca6f21048fafd2c8e47d7
kawi <kawi@kawi-PC>
2015-06-25 10:39:05 +0200

    Interpret Dialog, Speicheformat Dialog

--- src/gui/InputPanel.java
+++ src/gui/InputPanel.java
@@ -17,6 +17,7 @@ import javax.swing.border.TitledBorder;
 import javax.swing.border.EtchedBorder;
 import javax.swing.JCheckBox;
 import java.awt.Component;
+import javax.swing.border.BevelBorder;
 
 public class InputPanel extends JPanel implements ActionListener
 {
@@ -29,23 +30,25 @@ public class InputPanel extends JPanel implements ActionListener
 	private JTextField txfAlbum;
 	private JTextField txfErsterscheinung;
 	private JTextField txfGenre;
+	private JCheckBox chkbxLive;
+	private Font fontStd = new Font("Tahoma", Font.PLAIN, 14);
+	private BevelBorder brdBevel = new BevelBorder(BevelBorder.LOWERED);
 	
 	
 	public InputPanel()
 	{
-		//super();
 		setAlignmentY(Component.TOP_ALIGNMENT);
 		
-		setBorder(new TitledBorder(new EtchedBorder(EtchedBorder.LOWERED, null, null), "CD",TitledBorder.LEADING , TitledBorder.TOP, null, null));		
+		setBorder(new TitledBorder(new EtchedBorder(EtchedBorder.LOWERED, null, null), "Musik",TitledBorder.LEADING , TitledBorder.TOP, null, null));		
 		GridBagLayout gbl = new GridBagLayout();
-		gbl.columnWidths = new int[]{30, 0, 0, 0, 0};
+		gbl.columnWidths = new int[] {30, 0, 200, 30, 0};
 		gbl.rowHeights = new int[] {30, 0, 0, 0, 0, 0, 0, 0, 0};
 		gbl.columnWeights = new double[]{0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
 		gbl.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
 		setLayout(gbl);
 		
 		JLabel lblTitel = new JLabel("Titel");
-		lblTitel.setFont(new Font("Tahoma", Font.PLAIN, 12));
+		lblTitel.setFont(fontStd);
 		GridBagConstraints gbc_lblTitel = new GridBagConstraints();
 		gbc_lblTitel.insets = new Insets(0, 0, 10, 10);
 		gbc_lblTitel.anchor = GridBagConstraints.WEST;
@@ -54,18 +57,19 @@ public class InputPanel extends JPanel implements ActionListener
 		add(lblTitel, gbc_lblTitel);
 		
 		txfTitel = new JTextField();
-		txfTitel.setFont(new Font("Tahoma", Font.PLAIN, 12));
+		txfTitel.setBorder(brdBevel);
+		txfTitel.setFont(fontStd);
 		GridBagConstraints gbc_txfTitel = new GridBagConstraints();
+		gbc_txfTitel.fill = GridBagConstraints.HORIZONTAL;
 		gbc_txfTitel.anchor = GridBagConstraints.WEST;
 		gbc_txfTitel.insets = new Insets(0, 0, 10, 10);
 		gbc_txfTitel.gridx = 2;
 		gbc_txfTitel.gridy = 1;
-		txfTitel.setColumns(20);
 		add(txfTitel, gbc_txfTitel);
 		
 		
 		JLabel lblInterpret = new JLabel("Interpret");
-		lblInterpret.setFont(new Font("Tahoma", Font.PLAIN, 12));
+		lblInterpret.setFont(fontStd);
 		GridBagConstraints gbc_lblInterpret = new GridBagConstraints();
 		gbc_lblInterpret.insets = new Insets(0, 0, 10, 10);
 		gbc_lblInterpret.anchor = GridBagConstraints.WEST;
@@ -74,17 +78,19 @@ public class InputPanel extends JPanel implements ActionListener
 		add(lblInterpret, gbc_lblInterpret);
 		
 		txfInterpret = new JTextField();
-		txfInterpret.setFont(new Font("Tahoma", Font.PLAIN, 12));
+		txfInterpret.setBorder(brdBevel);
+		txfInterpret.setFont(fontStd);
 		GridBagConstraints gbc_txfInterpret = new GridBagConstraints();
+		gbc_txfInterpret.fill = GridBagConstraints.HORIZONTAL;
 		gbc_txfInterpret.anchor = GridBagConstraints.WEST;
 		gbc_txfInterpret.insets = new Insets(0, 0, 10, 10);
 		gbc_txfInterpret.gridx = 2;
 		gbc_txfInterpret.gridy = 2;
 		add(txfInterpret, gbc_txfInterpret);
-		txfInterpret.setColumns(20);
+		
 		
 		JLabel lblMitwirkende = new JLabel("Mitwirkende");
-		lblMitwirkende.setFont(new Font("Tahoma", Font.PLAIN, 12));
+		lblMitwirkende.setFont(fontStd);
 		GridBagConstraints gbc_lblMitwirkende = new GridBagConstraints();
 		gbc_lblMitwirkende.anchor = GridBagConstraints.WEST;
 		gbc_lblMitwirkende.insets = new Insets(0, 0, 10, 10);
@@ -93,17 +99,19 @@ public class InputPanel extends JPanel implements ActionListener
 		add(lblMitwirkende, gbc_lblMitwirkende);
 		
 		txfMitwirkende = new JTextField();
-		txfMitwirkende.setFont(new Font("Tahoma", Font.PLAIN, 12));
+		txfMitwirkende.setBorder(brdBevel);
+		txfMitwirkende.setFont(fontStd);
 		GridBagConstraints gbc_txfMitwirkende = new GridBagConstraints();
+		gbc_txfMitwirkende.fill = GridBagConstraints.HORIZONTAL;
 		gbc_txfMitwirkende.anchor = GridBagConstraints.WEST;
 		gbc_txfMitwirkende.insets = new Insets(0,0,10,10);
 		gbc_txfMitwirkende.gridx = 2;
 		gbc_txfMitwirkende.gridy = 3;
-		txfMitwirkende.setColumns(20);
+		
 		add(txfMitwirkende, gbc_txfMitwirkende);
 		
 		JLabel lblAlbum = new JLabel("Album");
-		lblAlbum.setFont(new Font("Tahoma", Font.PLAIN, 12));
+		lblAlbum.setFont(fontStd);
 		GridBagConstraints gbc_lblAlbum = new GridBagConstraints();
 		gbc_lblAlbum.insets = new Insets(0, 0, 10, 10);
 		gbc_lblAlbum.anchor = GridBagConstraints.WEST;
@@ -112,8 +120,10 @@ public class InputPanel extends JPanel implements ActionListener
 		add(lblAlbum, gbc_lblAlbum);
 		
 		txfAlbum = new JTextField();
-		txfAlbum.setFont(new Font("Tahoma", Font.PLAIN, 12));
+		txfAlbum.setBorder(brdBevel);
+		txfAlbum.setFont(fontStd);
 		GridBagConstraints gbc_txfAlbum = new GridBagConstraints();
+		gbc_txfAlbum.fill = GridBagConstraints.HORIZONTAL;
 		gbc_txfAlbum.anchor = GridBagConstraints.WEST;
 		gbc_txfAlbum.insets = new Insets(0, 0, 10, 10);
 		gbc_txfAlbum.gridx = 2;
@@ -123,7 +133,7 @@ public class InputPanel extends JPanel implements ActionListener
 		
 				
 		JLabel lblGenre = new JLabel("Genre");
-		lblGenre.setFont(new Font("Tahoma", Font.PLAIN, 12));
+		lblGenre.setFont(fontStd);
 		GridBagConstraints gbc_lblGenre = new GridBagConstraints();
 		gbc_lblGenre.anchor = GridBagConstraints.WEST;
 		gbc_lblGenre.insets = new Insets(0, 0, 10, 10);
@@ -132,8 +142,10 @@ public class InputPanel extends JPanel implements ActionListener
 		add(lblGenre, gbc_lblGenre);
 		
 		txfGenre = new JTextField();
-		txfGenre.setFont(new Font("Tahoma", Font.PLAIN, 12));
+		txfGenre.setBorder(brdBevel);
+		txfGenre.setFont(fontStd);
 		GridBagConstraints gbc_txfGenre = new GridBagConstraints();
+		gbc_txfGenre.fill = GridBagConstraints.HORIZONTAL;
 		gbc_txfGenre.anchor = GridBagConstraints.WEST;
 		gbc_txfGenre.insets = new Insets(0, 0, 10, 10);
 		gbc_txfGenre.gridx = 2;
@@ -142,7 +154,7 @@ public class InputPanel extends JPanel implements ActionListener
 		txfGenre.setColumns(20);
 		
 		JLabel lblErsterscheinung = new JLabel("Erstausgabe");
-		lblErsterscheinung.setFont(new Font("Tahoma", Font.PLAIN, 12));
+		lblErsterscheinung.setFont(fontStd);
 		GridBagConstraints gbc_lblErsterscheinung = new GridBagConstraints();
 		gbc_lblErsterscheinung.anchor = GridBagConstraints.WEST;
 		gbc_lblErsterscheinung.insets = new Insets(0, 0, 10, 10);
@@ -151,8 +163,10 @@ public class InputPanel extends JPanel implements ActionListener
 		add(lblErsterscheinung, gbc_lblErsterscheinung);
 		
 		txfErsterscheinung = new JTextField();
-		txfErsterscheinung.setFont(new Font("Tahoma", Font.PLAIN, 12));
+		txfErsterscheinung.setBorder(brdBevel);
+		txfErsterscheinung.setFont(fontStd);
 		GridBagConstraints gbc_txfErsterscheinung = new GridBagConstraints();
+		gbc_txfErsterscheinung.fill = GridBagConstraints.HORIZONTAL;
 		gbc_txfErsterscheinung.anchor = GridBagConstraints.WEST;
 		gbc_txfErsterscheinung.insets = new Insets(0, 0, 10, 10);
 		gbc_txfErsterscheinung.gridx = 2;
@@ -160,9 +174,9 @@ public class InputPanel extends JPanel implements ActionListener
 		add(txfErsterscheinung, gbc_txfErsterscheinung);
 		txfErsterscheinung.setColumns(20);
 		
-		JCheckBox chkbxLive = new JCheckBox("Live?");
+		chkbxLive = new JCheckBox("Live?");
 		chkbxLive.setActionCommand("Live");
-		chkbxLive.setFont(new Font("Tahoma", Font.PLAIN, 12));
+		chkbxLive.setFont(fontStd);
 		GridBagConstraints gbc_chkbxLive = new GridBagConstraints();
 		gbc_chkbxLive.anchor = GridBagConstraints.WEST;
 		gbc_chkbxLive.insets = new Insets(0, 0, 10, 10);
--- src/gui/Startfenster.java
+++ src/gui/Startfenster.java
@@ -13,19 +13,24 @@ import java.awt.Font;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.Insets;
-import java.awt.Panel;
-
 import javax.swing.JPanel;
 import javax.swing.JButton;
-import javax.swing.SwingConstants;
-
 import java.awt.FlowLayout;
+import javax.swing.JComboBox;
+import javax.swing.JLabel;
 
 public class Startfenster extends JFrame implements ActionListener
 {
 	
 	private static final long serialVersionUID = 1L;
 	Container cp;
+	private String[] selectMusik = {"anzeigen","anlegen"};
+	private Font fontStd = new Font("Tahoma", Font.PLAIN, 14);
+	private JPanel 	buttonPanel, southPanel;
+	private JButton btnSpeichern, btnAbbrechen;
+	private JComboBox cbxMusik;
+	private JLabel lblMusik;
+					
 
 	public Startfenster()							
 	{
@@ -33,54 +38,73 @@ public class Startfenster extends JFrame implements ActionListener
 		cp = getContentPane();
 		cp.setLayout(new BorderLayout());
 				
-		JPanel buttonPanel = new JPanel();
+		buttonPanel = new JPanel();
 		
 		GridBagLayout gbl_buttonPanel = new GridBagLayout();
 		gbl_buttonPanel.columnWidths = new int[] {100, 0};
 		gbl_buttonPanel.rowHeights = new int[] {0, 0, 0, 30, 0};
-		gbl_buttonPanel.columnWeights = new double[]{0.0, Double.MIN_VALUE};
+		gbl_buttonPanel.columnWeights = new double[]{1.0, Double.MIN_VALUE};
 		gbl_buttonPanel.rowWeights = new double[]{0.0, 0.0, 0.0, Double.MIN_VALUE};
 		buttonPanel.setLayout(gbl_buttonPanel);
 		
-		JButton btnMusik = new JButton("Musik");
-		btnMusik.setPreferredSize(new Dimension(100, 25));
-		btnMusik.setFont(new Font("Tahoma", Font.PLAIN, 12));
-		GridBagConstraints gbc_btnMusik = new GridBagConstraints();
-		gbc_btnMusik.anchor = GridBagConstraints.WEST;
-		gbc_btnMusik.insets = new Insets(10, 10, 10, 10);
-		gbc_btnMusik.gridx = 0;
-		gbc_btnMusik.gridy = 0;
-		buttonPanel.add(btnMusik, gbc_btnMusik);		
+//		JButton btnMusik = new JButton("Musik");
+//		btnMusik.setPreferredSize(new Dimension(100, 25));
+//		btnMusik.setFont(fontStd);
+//		GridBagConstraints gbc_btnMusik = new GridBagConstraints();
+//		gbc_btnMusik.anchor = GridBagConstraints.WEST;
+//		gbc_btnMusik.insets = new Insets(10, 10, 10, 10);
+//		gbc_btnMusik.gridx = 0;
+//		gbc_btnMusik.gridy = 0;
+//		buttonPanel.add(btnMusik, gbc_btnMusik);		
 		
-		JButton btnVideo = new JButton("Video");
-		btnVideo.setPreferredSize(new Dimension(100, 25));
-		btnVideo.setFont(new Font("Tahoma", Font.PLAIN, 12));
-		GridBagConstraints gbc_btnVideo = new GridBagConstraints();
-		gbc_btnVideo.anchor = GridBagConstraints.WEST;
-		gbc_btnVideo.insets = new Insets(0, 10, 10, 10);
-		gbc_btnVideo.gridx = 0;
-		gbc_btnVideo.gridy = 1;
-		buttonPanel.add(btnVideo, gbc_btnVideo);		
-		
-		JButton btnBuecher = new JButton("Bücher");
-		btnBuecher.setPreferredSize(new Dimension(100, 25));
-		btnBuecher.setFont(new Font("Tahoma", Font.PLAIN, 12));
-		GridBagConstraints gbc_btnNewButton = new GridBagConstraints();
-		gbc_btnNewButton.insets = new Insets(0, 10, 10, 10);
-		gbc_btnNewButton.anchor = GridBagConstraints.WEST;
-		gbc_btnNewButton.gridx = 0;
-		gbc_btnNewButton.gridy = 2;
-		buttonPanel.add(btnBuecher, gbc_btnNewButton);
+//		JButton btnVideo = new JButton("Video");
+//		btnVideo.setPreferredSize(new Dimension(100, 25));
+//		btnVideo.setFont(new Font("Tahoma", Font.PLAIN, 12));
+//		GridBagConstraints gbc_btnVideo = new GridBagConstraints();
+//		gbc_btnVideo.anchor = GridBagConstraints.WEST;
+//		gbc_btnVideo.insets = new Insets(0, 10, 10, 10);
+//		gbc_btnVideo.gridx = 0;
+//		gbc_btnVideo.gridy = 1;
+//		buttonPanel.add(btnVideo, gbc_btnVideo);		
+//		
+//		JButton btnBuecher = new JButton("Bücher");
+//		btnBuecher.setPreferredSize(new Dimension(100, 25));
+//		btnBuecher.setFont(new Font("Tahoma", Font.PLAIN, 12));
+//		GridBagConstraints gbc_btnNewButton = new GridBagConstraints();
+//		gbc_btnNewButton.insets = new Insets(0, 10, 10, 10);
+//		gbc_btnNewButton.anchor = GridBagConstraints.WEST;
+//		gbc_btnNewButton.gridx = 0;
+//		gbc_btnNewButton.gridy = 2;
+//		buttonPanel.add(btnBuecher, gbc_btnNewButton);
 		
 		
 		cp.add(buttonPanel, BorderLayout.WEST);
+		
+		lblMusik = new JLabel("Musik");
+		lblMusik.setFont(fontStd);
+		GridBagConstraints gbc_lblMusik = new GridBagConstraints();
+		gbc_lblMusik.insets = new Insets(0, 10, 10, 10);
+		gbc_lblMusik.gridx = 0;
+		gbc_lblMusik.gridy = 1;
+		buttonPanel.add(lblMusik, gbc_lblMusik);
+		
+		
+		cbxMusik = new JComboBox(selectMusik);
+		cbxMusik.setFont(fontStd);
+		
+		GridBagConstraints gbc_cbxMusik = new GridBagConstraints();
+		gbc_cbxMusik.insets = new Insets(0, 10, 10, 10);
+		gbc_cbxMusik.fill = GridBagConstraints.HORIZONTAL;
+		gbc_cbxMusik.gridx = 0;
+		gbc_cbxMusik.gridy = 2;
+		buttonPanel.add(cbxMusik, gbc_cbxMusik);
 		
 		
 		InputPanel input = new InputPanel();
 		input.setAlignmentY(Component.TOP_ALIGNMENT);
 		cp.add(input, BorderLayout.CENTER);
 		
-		JPanel southPanel = new JPanel();
+		southPanel = new JPanel();
 		FlowLayout flowLayout = (FlowLayout) southPanel.getLayout();
 		flowLayout.setVgap(10);
 		flowLayout.setHgap(10);
@@ -88,10 +112,10 @@ public class Startfenster extends JFrame implements ActionListener
 		
 		
 		
-		JButton btnSpeichern = new JButton("speichern");
+		btnSpeichern = new JButton("speichern");
 		southPanel.add(btnSpeichern);
 		
-		JButton btnAbbrechen = new JButton("abbrechen");
+		btnAbbrechen = new JButton("abbrechen");
 		southPanel.add(btnAbbrechen);
 		
 		cp.add(southPanel, BorderLayout.SOUTH);
@@ -107,13 +131,11 @@ public class Startfenster extends JFrame implements ActionListener
 		mv.setDefaultCloseOperation(EXIT_ON_CLOSE);
 		mv.setLocationRelativeTo(null);
 		mv.setResizable(false);
-		
-	
-
 	}
 
 	@Override
-	public void actionPerformed(ActionEvent arg0) {
+	public void actionPerformed(ActionEvent arg0) 
+	{
 		//  XXX (JW) Auto-generated method stub
 	}
 
