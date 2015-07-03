1f254bb24f8c4b27723972560f80d012ae01868a
jensw2015 <jenswiede@hotmail.de>
2015-07-03 10:21:50 +0200

    GUI Aenderungen

---
+++ medienverwaltung/src/gui/MusikEingabePanel.java
@@ -0,0 +1,234 @@
+package gui;
+
+import javax.swing.JPanel;
+import javax.swing.JLabel;
+
+import java.awt.BorderLayout;
+import java.awt.GridBagLayout;
+import java.awt.GridBagConstraints;
+import java.awt.Insets;
+
+import javax.swing.JTextField;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JCheckBox;
+
+import java.awt.Component;
+
+import javax.swing.JButton;
+import javax.swing.SwingConstants;
+import java.awt.FlowLayout;
+
+public class MusikEingabePanel extends JPanel implements ActionListener {
+	
+	private static final long serialVersionUID = 1L;
+	
+	private JTextField 		txfTitel;
+	private JTextField 		txfMitwirkende;
+	private JTextField 		txfErsterscheinung;
+	private JCheckBox 		chkbxLive;
+	private JButton 		btnSpeicherort;
+	private JButton 		btnInterpret;
+	private JButton 		btnAbbrechen;
+	private JButton 		btnSpeichern;
+	private JLabel			lblMusikEingabe;
+	private JLabel 			lblInterpret;
+	private JLabel 			lblSpeicherort;
+	
+	public MusikEingabePanel() {
+		setAlignmentY(Component.TOP_ALIGNMENT);
+		createMusikEingabePanel();
+
+		
+		btnInterpret.addActionListener(this);
+		btnSpeicherort.addActionListener(this);
+		btnSpeichern.addActionListener(this);
+		btnAbbrechen.addActionListener(this);
+	}
+	
+	private void createMusikEingabePanel()	{
+		
+		this.setLayout(new BorderLayout());
+		
+		JPanel pnlNorth		= new JPanel();
+		JPanel pnlCenter	= new JPanel();
+		JPanel pnlSouth		= new JPanel();
+		
+		pnlNorth.setBorder(StaticComponents.BORDER_PANEL);
+		pnlCenter.setBorder(StaticComponents.BORDER_PANEL);
+		
+		this.add(pnlNorth, BorderLayout.NORTH);
+		this.add(pnlCenter, BorderLayout.CENTER);
+		this.add(pnlSouth, BorderLayout.SOUTH);
+		
+		// Header
+		lblMusikEingabe = new JLabel("Musik Eingabe");
+		lblMusikEingabe.setFont(StaticComponents.FONT_TITLE);
+		pnlNorth.add(lblMusikEingabe);
+			
+		// creating BorderLayout.CENTER Components
+		GridBagLayout gbl = new GridBagLayout();
+		gbl.columnWidths = new int[] {30, 0, 0, 0, 30, 0};
+		gbl.rowHeights = new int[] {30, 0, 0, 0, 0, 0, 0, 0};
+		gbl.columnWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
+		gbl.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
+		pnlCenter.setLayout(gbl);
+		
+		JLabel lblTitel = new JLabel("Titel");
+		StaticComponents.setFontLabel(lblTitel);
+		GridBagConstraints gbc_lblTitel = new GridBagConstraints();
+		gbc_lblTitel.insets = new Insets(0, 0, 10, 10);
+		gbc_lblTitel.anchor = GridBagConstraints.WEST;
+		gbc_lblTitel.gridx = 1;
+		gbc_lblTitel.gridy = 1;
+		pnlCenter.add(lblTitel, gbc_lblTitel);
+		
+		txfTitel = new JTextField();
+		StaticComponents.setFontBorderTextField(txfTitel);
+		GridBagConstraints gbc_txfTitel = new GridBagConstraints();
+		gbc_txfTitel.gridwidth = 2;
+		gbc_txfTitel.fill = GridBagConstraints.BOTH;
+		gbc_txfTitel.anchor = GridBagConstraints.WEST;
+		gbc_txfTitel.insets = new Insets(0, 0, 10, 10);
+		gbc_txfTitel.gridx = 2;
+		gbc_txfTitel.gridy = 1;
+		pnlCenter.add(txfTitel, gbc_txfTitel);
+		
+		JLabel lblMitwirkende = new JLabel("Mitwirkende");
+		StaticComponents.setFontLabel(lblMitwirkende);
+		GridBagConstraints gbc_lblMitwirkende = new GridBagConstraints();
+		gbc_lblMitwirkende.anchor = GridBagConstraints.WEST;
+		gbc_lblMitwirkende.insets = new Insets(0, 0, 10, 10);
+		gbc_lblMitwirkende.gridx = 1;
+		gbc_lblMitwirkende.gridy = 2;
+		pnlCenter.add(lblMitwirkende, gbc_lblMitwirkende);
+		
+		txfMitwirkende = new JTextField();
+		StaticComponents.setFontBorderTextField(txfMitwirkende);
+		GridBagConstraints gbc_txfMitwirkende = new GridBagConstraints();
+		gbc_txfMitwirkende.gridwidth = 2;
+		gbc_txfMitwirkende.fill = GridBagConstraints.BOTH;
+		gbc_txfMitwirkende.anchor = GridBagConstraints.WEST;
+		gbc_txfMitwirkende.insets = new Insets(0,0,10,10);
+		gbc_txfMitwirkende.gridx = 2;
+		gbc_txfMitwirkende.gridy = 2;
+		pnlCenter.add(txfMitwirkende, gbc_txfMitwirkende);
+		
+		JLabel lblErsterscheinung = new JLabel("Erstausgabe");
+		StaticComponents.setFontLabel(lblErsterscheinung);
+		GridBagConstraints gbc_lblErsterscheinung = new GridBagConstraints();
+		gbc_lblErsterscheinung.anchor = GridBagConstraints.WEST;
+		gbc_lblErsterscheinung.insets = new Insets(0, 0, 10, 10);
+		gbc_lblErsterscheinung.gridx = 1;
+		gbc_lblErsterscheinung.gridy = 3;
+		pnlCenter.add(lblErsterscheinung, gbc_lblErsterscheinung);
+		
+		txfErsterscheinung = new JTextField();
+		StaticComponents.setFontBorderTextField(txfErsterscheinung);
+		GridBagConstraints gbc_txfErsterscheinung = new GridBagConstraints();
+		gbc_txfErsterscheinung.fill = GridBagConstraints.BOTH;
+		gbc_txfErsterscheinung.anchor = GridBagConstraints.WEST;
+		gbc_txfErsterscheinung.insets = new Insets(0, 0, 10, 10);
+		gbc_txfErsterscheinung.gridx = 2;
+		gbc_txfErsterscheinung.gridy = 3;
+		pnlCenter.add(txfErsterscheinung, gbc_txfErsterscheinung);
+		txfErsterscheinung.setColumns(15);
+		
+		chkbxLive = new JCheckBox("Live");
+		chkbxLive.setMargin(new Insets(2, 0, 2, 2));
+		chkbxLive.setIconTextGap(10);
+		chkbxLive.setHorizontalTextPosition(SwingConstants.LEFT);
+		chkbxLive.setActionCommand("Live");
+		StaticComponents.setFontLabel(chkbxLive);
+		GridBagConstraints gbc_chkbxLive = new GridBagConstraints();
+		gbc_chkbxLive.anchor = GridBagConstraints.WEST;
+		gbc_chkbxLive.insets = new Insets(0, 0, 10, 10);
+		gbc_chkbxLive.gridx = 3;
+		gbc_chkbxLive.gridy = 3;
+		pnlCenter.add(chkbxLive, gbc_chkbxLive);
+		
+		btnInterpret = new JButton("Interpret");
+		btnInterpret.setFont(StaticComponents.FONT_BUTTON);
+		GridBagConstraints gbc_btnInterpret = new GridBagConstraints();
+		gbc_btnInterpret.fill = GridBagConstraints.HORIZONTAL;
+		gbc_btnInterpret.insets = new Insets(0, 0, 10, 10);
+		gbc_btnInterpret.gridx = 1;
+		gbc_btnInterpret.gridy = 4;
+		pnlCenter.add(btnInterpret, gbc_btnInterpret);
+		
+		lblInterpret = new JLabel();
+		StaticComponents.setFontBorderLabel(lblInterpret);
+		GridBagConstraints gbc_lblInterpret = new GridBagConstraints();
+		gbc_lblInterpret.gridwidth = 2;
+		gbc_lblInterpret.fill = GridBagConstraints.BOTH;
+		gbc_lblInterpret.anchor = GridBagConstraints.WEST;
+		gbc_lblInterpret.insets = new Insets(0, 0, 10, 10);
+		gbc_lblInterpret.gridx = 2;
+		gbc_lblInterpret.gridy = 4;
+		pnlCenter.add(lblInterpret, gbc_lblInterpret);
+		
+		btnSpeicherort = new JButton("Speicherort");
+		btnSpeicherort.setFont(StaticComponents.FONT_BUTTON);
+		GridBagConstraints gbc_btnSpeicherformat = new GridBagConstraints();
+		gbc_btnSpeicherformat.fill = GridBagConstraints.HORIZONTAL;
+		gbc_btnSpeicherformat.insets = new Insets(0, 0, 10, 10);
+		gbc_btnSpeicherformat.gridx = 1;
+		gbc_btnSpeicherformat.gridy = 5;
+		pnlCenter.add(btnSpeicherort, gbc_btnSpeicherformat);
+		
+		lblSpeicherort = new JLabel();
+		StaticComponents.setFontBorderLabel(lblSpeicherort);
+		GridBagConstraints gbc_lblSpeicherort = new GridBagConstraints();
+		gbc_lblSpeicherort.gridwidth = 2;
+		gbc_lblSpeicherort.fill = GridBagConstraints.BOTH;
+		gbc_lblSpeicherort.anchor = GridBagConstraints.WEST;
+		gbc_lblSpeicherort.insets = new Insets(0, 0, 10, 10);
+		gbc_lblSpeicherort.gridx = 2;
+		gbc_lblSpeicherort.gridy = 5;
+		pnlCenter.add(lblSpeicherort, gbc_lblSpeicherort);
+				
+		// creating BorderLayout.SOUTH Components
+		FlowLayout fl_pnlSouth = (FlowLayout) pnlSouth.getLayout();
+		fl_pnlSouth.setVgap(10);
+		fl_pnlSouth.setHgap(10);
+		fl_pnlSouth.setAlignment(FlowLayout.RIGHT);
+		pnlSouth.setBorder(StaticComponents.BORDER_PANEL);
+		
+		btnSpeichern = new JButton("speichern");
+		btnSpeichern.setFont(StaticComponents.FONT_BUTTON);
+		pnlSouth.add(btnSpeichern);
+		
+		btnAbbrechen = new JButton("abbrechen");
+		btnAbbrechen.setFont(StaticComponents.FONT_BUTTON);
+		pnlSouth.add(btnAbbrechen);
+		
+		
+		
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) 
+	{
+		Object source = e.getSource();
+		
+		if (source == btnInterpret)
+		{
+			
+		}
+		else if (source == btnSpeicherort)
+		{
+			
+		}
+		else if (source == btnSpeichern)
+		{
+			
+		}
+		else if (source == btnAbbrechen)
+		{
+			
+		}
+		
+	}
+}
---
+++ medienverwaltung/src/gui/dialog/Speicherort.java
@@ -0,0 +1,188 @@
+package gui.dialog;
+
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
+import java.awt.Window;
+import java.awt.event.ActionEvent;
+
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+import data.SpeicherFormatInterface;
+import data.speicherformate.logic.SpeicherFormateLogik;
+import gui.ReturningDialog;
+import gui.StaticComponents;
+
+/**
+ * Dialog zur Eingabe eines neuen Speicherformates
+ * @author Bernd Schmidt Logikimplementierung
+ * @author Jens Wiede (design)
+ *
+ */
+public class Speicherort extends ReturningDialog<SpeicherFormatInterface> {
+	private	JTextField	txfDatentraeger;
+	private	JTextField	txfPfad;
+	private	JTextField	txfDateiformat;
+	private	JTextField	txfQualitaet;
+	private	JButton		btnSpeichern;
+	private	JButton		btnAbbrechen;
+	private	int			titelId;
+	
+	
+	public Speicherort(Window parent, int titelId) {
+		super(parent, "Speicherort", 400, 300);
+		this.titelId = titelId;
+		this.createDialogContent();
+	}
+	
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		if (arg0.getSource() == btnAbbrechen) {
+			dialog.dispose();
+		} else if (arg0.getSource() == btnSpeichern) {
+			SpeicherFormateLogik logik = new SpeicherFormateLogik();
+			logik.setTitelId(titelId);
+			boolean errors = false;
+			if (logik.createDigitalMusik(txfDatentraeger.getText(), txfPfad.getText(), txfDateiformat.getText(), txfQualitaet.getText())) {
+				if (logik.write()) {
+					setObject(logik.getObject());
+					dialog.dispose();
+				} else {
+					errors = true;
+				}
+			} else {
+				errors = true;
+			}
+			if (errors) {
+				FehlerDialog fehlerDialog = new FehlerDialog(null, logik.getErrors());
+				fehlerDialog.setAlwaysOnTop(true);
+				fehlerDialog.setVisible(true);
+			}
+		}
+	}
+
+	@Override
+	protected JPanel createDialogContent() 
+	{
+		// Creating MainPanel and Subpanels
+		JPanel display		= 	new JPanel(new BorderLayout());
+		JPanel pnlNorth		=	new JPanel();
+		JPanel pnlCenter	=	new JPanel();
+		JPanel pnlSouth		=	new JPanel();
+		display.add(pnlNorth, BorderLayout.NORTH);
+		display.add(pnlCenter, BorderLayout.CENTER);
+		display.add(pnlSouth, BorderLayout.SOUTH);
+		
+		// Header
+		FlowLayout fl_pnlNorth = new FlowLayout();
+		fl_pnlNorth.setAlignment(FlowLayout.CENTER);
+		pnlNorth.setLayout(fl_pnlNorth);
+		
+		JLabel lblHeader = new JLabel("Speicherort");
+		lblHeader.setFont(StaticComponents.FONT_TITLE);
+		pnlNorth.add(lblHeader);
+		
+		// Main
+		GridBagLayout gbl = new GridBagLayout();
+		gbl.columnWidths = new int[] {30, 0, 200, 30};
+		gbl.rowHeights = new int[] {30, 0, 0, 0, 0, 0, 0, 30};
+		gbl.columnWeights = new double[]{0.0, 0.0, 0.0, Double.MIN_VALUE};
+		gbl.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
+		pnlCenter.setLayout(gbl);
+		
+		JLabel lblDatentraeger = new JLabel("Datentr\u00E4ger");
+		StaticComponents.setFontLabel(lblDatentraeger);
+		GridBagConstraints gbc_lblDatentraeger = new GridBagConstraints();
+		gbc_lblDatentraeger.anchor = GridBagConstraints.WEST;
+		gbc_lblDatentraeger.insets = new Insets(0, 0, 10, 10);
+		gbc_lblDatentraeger.gridx = 1;
+		gbc_lblDatentraeger.gridy = 2;
+		pnlCenter.add(lblDatentraeger, gbc_lblDatentraeger);
+		
+		txfDatentraeger = new JTextField();
+		StaticComponents.setFontBorderTextField(txfDatentraeger);
+		GridBagConstraints gbc_txfDatentraeger = new GridBagConstraints();
+		gbc_txfDatentraeger.fill = GridBagConstraints.HORIZONTAL;
+		gbc_txfDatentraeger.insets = new Insets(0, 0, 10, 10);
+		gbc_txfDatentraeger.gridx = 2;
+		gbc_txfDatentraeger.gridy = 2;
+		pnlCenter.add(txfDatentraeger, gbc_txfDatentraeger);
+		
+		JLabel lblPfad = new JLabel("Pfad");
+		StaticComponents.setFontLabel(lblPfad);
+		GridBagConstraints gbc_lblPfad = new GridBagConstraints();
+		gbc_lblPfad.anchor = GridBagConstraints.WEST;
+		gbc_lblPfad.insets = new Insets(0, 0, 10, 10);
+		gbc_lblPfad.gridx = 1;
+		gbc_lblPfad.gridy = 3;
+		pnlCenter.add(lblPfad, gbc_lblPfad);
+		
+		txfPfad = new JTextField();
+		StaticComponents.setFontBorderTextField(txfPfad);
+		GridBagConstraints gbc_txfPfad = new GridBagConstraints();
+		gbc_txfPfad.fill = GridBagConstraints.HORIZONTAL;
+		gbc_txfPfad.insets = new Insets(0, 0, 10, 10);
+		gbc_txfPfad.gridx = 2;
+		gbc_txfPfad.gridy = 3;
+		pnlCenter.add(txfPfad, gbc_txfPfad);
+		
+		JLabel lblDateiformat = new JLabel("Dateiformat");
+		StaticComponents.setFontLabel(lblDateiformat);
+		GridBagConstraints gbc_lblDateiformat = new GridBagConstraints();
+		gbc_lblDateiformat.anchor = GridBagConstraints.WEST;
+		gbc_lblDateiformat.insets = new Insets(0, 0, 10, 10);
+		gbc_lblDateiformat.gridx = 1;
+		gbc_lblDateiformat.gridy = 4;
+		pnlCenter.add(lblDateiformat, gbc_lblDateiformat);
+		
+		txfDateiformat = new JTextField();
+		StaticComponents.setFontBorderTextField(txfDateiformat);
+		GridBagConstraints gbc_txfDateiformat = new GridBagConstraints();
+		gbc_txfDateiformat.fill = GridBagConstraints.HORIZONTAL;
+		gbc_txfDateiformat.insets = new Insets(0, 0, 10, 10);
+		gbc_txfDateiformat.gridx = 2;
+		gbc_txfDateiformat.gridy = 4;
+		pnlCenter.add(txfDateiformat, gbc_txfDateiformat);
+		
+		JLabel lblQualitaet = new JLabel("Qualität");
+		StaticComponents.setFontLabel(lblQualitaet);
+		GridBagConstraints gbc_lblQualitaet = new GridBagConstraints();
+		gbc_lblQualitaet.anchor = GridBagConstraints.WEST;
+		gbc_lblQualitaet.insets = new Insets(0, 0, 10, 10);
+		gbc_lblQualitaet.gridx = 1;
+		gbc_lblQualitaet.gridy = 5;
+		pnlCenter.add(lblQualitaet, gbc_lblQualitaet);
+		
+		txfQualitaet = new JTextField();
+		StaticComponents.setFontBorderTextField(txfQualitaet);
+		gbc_txfDateiformat = new GridBagConstraints();
+		gbc_txfDateiformat.fill = GridBagConstraints.HORIZONTAL;
+		gbc_txfDateiformat.insets = new Insets(0, 0, 10, 10);
+		gbc_txfDateiformat.gridx = 2;
+		gbc_txfDateiformat.gridy = 5;
+		pnlCenter.add(txfQualitaet, gbc_txfDateiformat);
+		
+		// Buttons
+		FlowLayout fl_pnlSouth = new FlowLayout();
+		fl_pnlSouth.setVgap(10);
+		fl_pnlSouth.setHgap(10);
+		fl_pnlSouth.setAlignment(FlowLayout.RIGHT);
+		pnlSouth.setLayout(fl_pnlSouth);
+		
+		btnSpeichern = new JButton("Speichern");
+		pnlSouth.add(btnSpeichern);
+		btnAbbrechen = new JButton("Abbrechen");
+		pnlSouth.add(btnAbbrechen);
+		
+		// Event Listener
+		btnSpeichern.addActionListener(this);
+		btnAbbrechen.addActionListener(this);
+		
+		return display;
+	}
+}
