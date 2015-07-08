799b3efe19e13723377cbbbe66aa2cd60a4d0119
Gamer <Gamer@Jasi-THINK>
2015-07-08 12:44:47 +0200

    GUI Listen update

--- src/gui/PersonenListePanel.java
+++ src/gui/PersonenListePanel.java
@@ -1,11 +1,14 @@
 package gui;
 
+import gui.dialog.FehlerDialog;
+import gui.dialog.PersonEingabeDialog;
 import gui.renderer.PersonListRenderer;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
+import javax.swing.JDialog;
 import javax.swing.JLabel;
 import javax.swing.JList;
 import javax.swing.JPanel;
@@ -15,8 +18,7 @@ import javax.swing.ScrollPaneConstants;
 import java.awt.BorderLayout;
 import java.awt.Component;
 import java.awt.Dimension;
-import java.awt.FlowLayout;
-import java.awt.GridLayout;
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.swing.JScrollPane;
@@ -24,13 +26,19 @@ import javax.swing.JScrollPane;
 import data.Person;
 import data.logic.PersonLogik;
 import enums.ErrorMessage;
+import enums.ErrorsGUI;
 
 public class PersonenListePanel extends JPanel implements ActionListener {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 6117444169258495955L;
 	private JLabel			lblHeader;
 	private JList<Person>	lstPersonList;
 	private JButton			btnBearbeiten;
 	private JButton			btnLoeschen;
-	private JScrollPane scrollPane;
+	private JScrollPane 	scrollPane;
+	private JPanel 			pnlCenter;
 	
 
 	public PersonenListePanel() {
@@ -46,7 +54,7 @@ public class PersonenListePanel extends JPanel implements ActionListener {
 		this.setLayout(new BorderLayout());
 		
 		JPanel pnlNorth		= new JPanel();
-		JPanel pnlCenter	= new JPanel();
+		pnlCenter			= new JPanel();
 		JPanel pnlSouth		= new JPanel();
 
 		pnlNorth.setBorder(StaticComponents.BORDER_PANEL);
@@ -63,7 +71,59 @@ public class PersonenListePanel extends JPanel implements ActionListener {
 		pnlNorth.add(lblHeader);
 		
 		//Center
-		// Main
+		generateList();
+		
+		
+		//Footer
+		btnBearbeiten = new JButton("Bearbeiten");
+		btnBearbeiten.setFont(StaticComponents.FONT_BUTTON);
+		pnlSouth.add(btnBearbeiten);
+		btnLoeschen = new JButton("Löschen");
+		btnLoeschen.setFont(StaticComponents.FONT_BUTTON);
+		pnlSouth.add(btnLoeschen);
+		
+		
+		
+	}
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		Object source = e.getSource();
+		
+		if (source == btnLoeschen)
+		{
+			if (lstPersonList.isSelectionEmpty()) {
+				List<ErrorMessage> errors = new ArrayList<ErrorMessage>();
+				errors.add(ErrorsGUI.NoSelection);
+				JDialog	errorDialog = new FehlerDialog(null, errors);
+				errorDialog.setVisible(true);
+				return;
+			}
+			Person selected = lstPersonList.getSelectedValue();
+			PersonLogik logik = new PersonLogik();
+			if (logik.loadObject(selected.getId()) && logik.delete()) {
+				generateList();
+			} else {
+				FehlerDialog dialogFehler = new FehlerDialog(null, logik.getErrors());
+				dialogFehler.setVisible(true);
+			}
+		}
+		else if (source == btnBearbeiten)
+		{
+			if (lstPersonList.isSelectionEmpty()) {
+				List<ErrorMessage> errors = new ArrayList<ErrorMessage>();
+				errors.add(ErrorsGUI.NoSelection);
+				JDialog	errorDialog = new FehlerDialog(null, errors);
+				errorDialog.setVisible(true);
+				return;
+			}
+			Person selecte = lstPersonList.getSelectedValue();
+			PersonEingabeDialog dialog = new PersonEingabeDialog(null);
+			dialog.setPerson(selecte);
+			dialog.display();
+			generateList();
+		}
+	}
+	private void generateList() {
 		PersonLogik		logik		=	new PersonLogik();
 		List<Person>	listData	=	logik.getAll();
 		
@@ -86,32 +146,7 @@ public class PersonenListePanel extends JPanel implements ActionListener {
 				pnlCenter.add(new JLabel(error.getErrorMessage()));
 			}
 		}
-		
-		
-		//Footer
-		btnBearbeiten = new JButton("Bearbeiten");
-		btnBearbeiten.setFont(StaticComponents.FONT_BUTTON);
-		pnlSouth.add(btnBearbeiten);
-		btnLoeschen = new JButton("Löschen");
-		btnLoeschen.setFont(StaticComponents.FONT_BUTTON);
-		pnlSouth.add(btnLoeschen);
-		
-		
-		
+		pnlCenter.revalidate();
+		pnlCenter.repaint();
 	}
-	@Override
-	public void actionPerformed(ActionEvent e) {
-		// TODO Auto-generated method stub
-		Object source = e.getSource();
-		
-		if (source == btnLoeschen)
-		{
-			Person selected = lstPersonList.getSelectedValue();
-		}
-		else if (source == btnBearbeiten)
-		{
-			Person selecte = lstPersonList.getSelectedValue();
-		}
-	}
-
 }
