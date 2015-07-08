9c65eebae36d60ddb93a867d92468e0f12429396
jensw2015 <kawi@kawi-PC>
2015-07-08 11:03:34 +0200

    Errror Dialog

--- src/enums/ErrorsGUI.java
+++ src/enums/ErrorsGUI.java
@@ -3,6 +3,7 @@ package enums;
 public enum ErrorsGUI implements ErrorMessage {
 	NoSelection("Es wurde kein Element der Liste ausgewaehlt."),
 	NoDateFormat("Das Datum entspricht nicht dem Format TT.MM.JJJJ"),
+	NoDataAvailable("Keine Daten verfügbar."),
 	;
 
 	private	String errorMessage;
